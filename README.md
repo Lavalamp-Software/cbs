23fed6bda1651b5d530ca1e5122f282bd8aead17
shabman <avia.shabbyman@gmail.com>
2022-11-20 00:39:09 +0000

    first push

---
+++ README.md
@@ -0,0 +1,9 @@
+# CBS (C/C++ Building System)
+
+-----------------
+
+###### Developed by shabman (Mustafa Malik) and Intervinn
+
+- Coming Soon...
+
+##### `Copyright (C) 2022 Lavalamp Software. See License for more.`
---
+++ src/cbs.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : cbs.cpp
+// Author      : Mustafa Malik
+// Version     :
+// Copyright   : Copyright (C) 2022 Lavalamp Software
+// Description : Entry point for application
+//============================================================================
+
+#include <iostream>
+
+#define s std::
+
+int main() {
+	s cout << "hi" << s endl; // prints hi
+	return 0;
+}
